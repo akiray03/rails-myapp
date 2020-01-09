6039d44c1d828422d986d3beac7dca72b9328339
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-09 14:47:44 +0900

    ユーザにrole情報を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :feedbacks
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  ROLE_CHOICES = %w(customer support)
 
   has_secure_password
 
@@ -10,4 +11,5 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+  validates :role, presence: true, inclusion: ROLE_CHOICES
 end
