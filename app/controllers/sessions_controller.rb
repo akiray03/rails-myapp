fc40f5150cf7b65d1d4d55563fd3eba07ef7b3b1
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-15 16:31:14 +0900

    ログインフォームを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    unless user
+      render :new
+      return
+    end
+
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
