f0ba4d305ef1771ea27a7f7387b5f464038fa4ab
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-15 19:42:03 +0900

    Feedback - FeedbackComment のリレーション追加

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :user
+  has_many :feedback_comments
 
   validates :title, length: {maximum: 100}
   validates :description, length: {maximum: 4000}
--- app/models/feedback_comment.rb
+++ app/models/feedback_comment.rb
@@ -1,2 +1,6 @@
 class FeedbackComment < ApplicationRecord
+  belongs_to :feedback
+  belongs_to :user
+
+  validates :body, presence: true, length: { maximum: 60000 }
 end
