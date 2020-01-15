36465c9bcff6b817e8192d1f51e8b99030c70edf
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-15 17:11:57 +0900

    Feedbacks#index に登録したユーザ名、最終更新日時を表示する

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -4,7 +4,7 @@ class FeedbacksController < ApplicationController
   # GET /feedbacks
   # GET /feedbacks.json
   def index
-    @feedbacks = Feedback.all
+    @feedbacks = Feedback.joins(:user).all.order(updated_at: :desc)
   end
 
   # GET /feedbacks/1
