5187cbadd3b16dc811fdb79556e0c73e06bc3177
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-16 12:37:04 +0900

    Feedback Admin 画面で feedback を編集できるようにする

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -22,8 +22,36 @@ class Admin::FeedbacksController < ApplicationController
   end
 
   def edit
+    @feedback = Feedback.find(params[:id])
+
+    unless current_user&.support?
+      flash[:notice] = "Cloud not access to feedback admin page."
+      redirect_to @feedback
+    end
   end
 
   def update
+    @feedback = Feedback.find(params[:id])
+
+    unless current_user&.support?
+      flash[:notice] = "Cloud not access to feedback admin page."
+      redirect_to @feedback
+      return
+    end
+
+    respond_to do |format|
+      if @feedback.update(feedback_params)
+        format.html { redirect_to admin_feedback_path(@feedback), notice: 'Feedback was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def feedback_params
+    params.require(:feedback).permit(:title, :description)
   end
 end
