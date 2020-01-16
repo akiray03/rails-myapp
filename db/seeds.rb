95cbbf896700b84acc374632f7c607bb1587ef6a
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-16 09:57:06 +0900

    raisl db:seed でテスト用ユーザを作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unless User.find_by(email: 'customer@example.com')
+  User.create(
+      {
+          name: 'Customer User',
+          email: 'customer@example.com',
+          password: 'password',
+          role: 'customer'
+      }
+  )
+end
+
+unless User.find_by(email: 'support@example.com')
+  User.create(
+      {
+          name: 'Support User',
+          email: 'support@example.com',
+          password: 'password',
+          role: 'support'
+      }
+  )
+end
