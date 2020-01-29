fcfd5fd67bbd6d95d7bae05b1f76e84628eeea8d
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-01-29 18:36:01 +0900

    Feedbackのpriority, statusを select box での選択式に変更

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,6 +2,11 @@ class Feedback < ApplicationRecord
   belongs_to :user
   has_many :feedback_comments
 
+  PRIORITY_CHOICES = %w(high middle low)
+  STATUS_CHOICES = %w(new designing developing released wontfix)
+
   validates :title, length: {maximum: 100}
   validates :description, length: {maximum: 4000}
+  validates :priority, allow_blank: true, inclusion: PRIORITY_CHOICES
+  validates :status, allow_blank: true, inclusion: STATUS_CHOICES
 end
